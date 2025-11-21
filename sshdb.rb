c8c79fc05f0c6792a5bce57ca9f2475e0b85769c
Riccardo Iaconelli <riccardo@kde.org>
2025-11-21 14:09:36 +0100

    Update sshdb.rb with correct SHA256 and license

--- sshdb.rb
+++ sshdb.rb
@@ -2,8 +2,8 @@ class Sshdb < Formula
   desc "Keyboard-first SSH library and launcher TUI"
   homepage "https://github.com/ruphy/sshdb"
   url "https://github.com/ruphy/sshdb/archive/refs/tags/v0.15.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_SHA256"
-  license "GPL-3.0-only"
+  sha256 "f0fed6beb31bc95fd75b7ed9e1dd0cd11a5588e3934b27d8b469049c91a27e57"
+  license "GPL-3.0-or-later"
   depends_on "rust" => :build
 
   def install
